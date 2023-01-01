d641e7259806687e325837e862ed32c28165602c
Alexander Hirdman <hirdman.alex@icloud.com>
2023-01-01 17:25:02 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Alexander Hirdman on 2023-01-01.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
